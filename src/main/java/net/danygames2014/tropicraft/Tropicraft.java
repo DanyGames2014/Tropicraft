a146bc7346c82eb168d6d0f7434c14bfa9bcb4a6
Daniel Fildán <fildand@centrum.cz>
2024-02-27 07:57:55 +0100

    Added Bamboo Stairs

--- src/main/java/net/danygames2014/tropicraft/Tropicraft.java
+++ src/main/java/net/danygames2014/tropicraft/Tropicraft.java
@@ -1,14 +1,13 @@
 package net.danygames2014.tropicraft;
 
 import net.danygames2014.tropicraft.block.BambooBlock;
-import net.danygames2014.tropicraft.block.BambooStairs;
+import net.danygames2014.tropicraft.block.template.StairsBlockTemplate;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.template.block.TemplateBlock;
-import net.modificationstation.stationapi.api.template.block.TemplateStairsBlock;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 
@@ -25,6 +24,6 @@ public class Tropicraft {
     public void registerBlocks(BlockRegistryEvent event){
         bambooBlock = new BambooBlock(NAMESPACE.id("bamboo_block")).setTranslationKey(NAMESPACE, "bamboo_block");
         bambooPlanksBlock = new TemplateBlock(NAMESPACE.id("bamboo_planks"), Material.WOOD).setTranslationKey(NAMESPACE, "bamboo_planks").setHardness(1.0F).setResistance(0.1F);
-        bambooStairs = new BambooStairs(NAMESPACE.id("bamboo_stairs"), bambooPlanksBlock).setTranslationKey(NAMESPACE, "bamboo_stairs").setHardness(1.0F).setResistance(0.1F);
+        bambooStairs = new StairsBlockTemplate(NAMESPACE.id("bamboo_stairs"), bambooPlanksBlock).setTranslationKey(NAMESPACE, "bamboo_stairs").setHardness(1.0F).setResistance(0.1F);
     }
 }
