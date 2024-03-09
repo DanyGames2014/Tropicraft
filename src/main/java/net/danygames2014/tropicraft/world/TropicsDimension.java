d7aea49de1b53683e454ca83512a657637fd72f9
Daniel Fildán <fildand@centrum.cz>
2024-03-09 08:11:02 +0100

    The Tropics Enter/Leaves Messages

--- src/main/java/net/danygames2014/tropicraft/world/TropicsDimension.java
+++ src/main/java/net/danygames2014/tropicraft/world/TropicsDimension.java
@@ -1,9 +1,30 @@
 package net.danygames2014.tropicraft.world;
 
+import net.danygames2014.tropicraft.Tropicraft;
+import net.minecraft.class_51;
 import net.minecraft.world.dimension.Dimension;
+import net.modificationstation.stationapi.api.client.world.dimension.TravelMessageProvider;
 
-public class TropicsDimension extends Dimension {
+public class TropicsDimension extends Dimension implements TravelMessageProvider {
     public TropicsDimension(int id) {
         this.id = id;
     }
+
+    @Override
+    public String getEnteringTranslationKey() {
+        return "dim.tropicraft:tropics.entering";
+    }
+
+    @Override
+    public String getLeavingTranslationKey() {
+        return "dim.tropicraft:tropics.leaving";
+    }
+
+    // Chunk Provider
+    @Override
+    public class_51 method_1772() {
+        return null;
+    }
+
+
 }
