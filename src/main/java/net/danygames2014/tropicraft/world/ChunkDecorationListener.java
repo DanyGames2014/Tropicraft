75d09b9bc4d536cac06b00e39bacf1d2a933d92e
Daniel Fildán <fildand@centrum.cz>
2024-03-21 10:28:43 +0100

    [WIP] Nearly working Bamboo Patch Feature

--- src/main/java/net/danygames2014/tropicraft/world/ChunkDecorationListener.java
+++ src/main/java/net/danygames2014/tropicraft/world/ChunkDecorationListener.java
@@ -1,11 +1,17 @@
 package net.danygames2014.tropicraft.world;
 
+import net.danygames2014.tropicraft.world.feature.BambooPatchFeature;
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.minecraft.block.Block;
-import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.event.world.gen.WorldGenEvent;
 
 public class ChunkDecorationListener {
+    public static BambooPatchFeature bambooPatchFeature = new BambooPatchFeature();
+
+    public static final boolean GENERATE_BAMBOO = true;
+
+    // Chance is 1/value
+    public static final int BAMBOO_GEN_CHANCE = 25;
+
     @EventListener
     public void decorate(WorldGenEvent.ChunkDecoration event) {
         if (event.world.dimension.id == 0) {
@@ -14,11 +20,14 @@ public class ChunkDecorationListener {
     }
 
     public void decorateOverworld(WorldGenEvent.ChunkDecoration event) {
-        int x = event.x + event.random.nextInt(0, 15);
-        int z = event.z + event.random.nextInt(0, 15);
-        int y = event.world.getTopY(x, z);
-        BlockState topBlock = event.world.getBlockState(x,y-1,z);
-        if(topBlock.)
-        event.world.setBlock(x, y, z, Block.DIAMOND_BLOCK.id);
+//        int x = event.x + event.random.nextInt(0, 15);
+//        int z = event.z + event.random.nextInt(0, 15);
+//        int topY = event.world.getTopY(x, z);
+        if(
+            (event.world.method_1781().method_1786(event.x, event.z) > 0.2D) && // Temperature is above 0.2
+            (event.random.nextInt(BAMBOO_GEN_CHANCE) == 0)) // Bamboo Gen Chance
+        {
+            bambooPatchFeature.generate(event.world, event.random, event.x + 8, 0, event.z + 8);
+        }
     }
 }
--- src/main/java/net/danygames2014/tropicraft/world/feature/BambooPatchFeature.java
+++ src/main/java/net/danygames2014/tropicraft/world/feature/BambooPatchFeature.java
@@ -1,13 +1,50 @@
 package net.danygames2014.tropicraft.world.feature;
 
+import net.danygames2014.tropicraft.Tropicraft;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.Feature;
 
 import java.util.Random;
 
 public class BambooPatchFeature extends Feature {
+    public static final int MIN_HEIGHT = 4;
+    public static final int MAX_HEIGHT = 11;
+
+    public static final int MIN_BAMBOO = 40;
+    public static final int MAX_BAMBOO = 100;
+
     @Override
-    public boolean generate(World world, Random random, int x, int y, int z) {
-        return false;
+    public boolean generate(World world, Random random, int xPos, int yPos, int zPos) {
+        int x = xPos + 8;
+        int z = zPos + 8;
+        int y = world.getTopY(x, z);
+
+        // If the block isn't air, return
+        if (!world.method_234(x, y, z)) {
+            return false;
+        }
+
+        // Calculate the amount and spread used for this patch
+        int amount = random.nextInt(MAX_BAMBOO - MIN_BAMBOO) + MIN_BAMBOO;
+        int spread = random.nextInt(3) - 1 + (int) (Math.sqrt(amount) / 2.0);
+
+        // Spawn <amount> bamboo stalks
+        for (int l = 0; l < amount; ++l) {
+            int bambooX = x + random.nextInt(spread);
+            int bambooZ = z + random.nextInt(spread);
+            int bambooY = world.getTopY(bambooX, bambooZ) - 1;
+            int height = random.nextInt(MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT;
+            BlockPos pos = new BlockPos(bambooX, bambooY, bambooZ);
+            for (int h = 0; h < height; ++h) {
+                pos = pos.up();
+                // If the block isn't air or bamboo can't grow on it, continue
+                if (!world.method_234(pos.x, pos.y, pos.z) || !Tropicraft.bambooShootBlock.canGrow(world, pos.x, pos.y, pos.z)) {
+                    continue;
+                }
+                world.setBlock(pos.x, pos.y, pos.z, Tropicraft.bambooShootBlock.id);
+            }
+        }
+        return true;
     }
 }
