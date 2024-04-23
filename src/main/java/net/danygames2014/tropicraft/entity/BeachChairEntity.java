a631f827b586aaee0b8527cbf81105aad0789aee
Daniel Fildán <fildand@centrum.cz>
2024-04-23 06:36:36 +0200

    [WIP] Beach Chair

--- src/main/java/net/danygames2014/tropicraft/entity/BeachChairEntity.java
+++ src/main/java/net/danygames2014/tropicraft/entity/BeachChairEntity.java
@@ -1,10 +1,10 @@
 package net.danygames2014.tropicraft.entity;
 
 import net.danygames2014.tropicraft.Tropicraft;
+import net.danygames2014.tropicraft.mixin.LivingEntityAccessor;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.server.entity.EntitySpawnDataProvider;
@@ -14,7 +14,7 @@ public class BeachChairEntity extends Entity implements EntitySpawnDataProvider 
 
     public BeachChairEntity(World world) {
         super(world);
-        this.setBoundingBoxSpacing(1.0F,1.0F);
+        this.setBoundingBoxSpacing(1.0F, 1.0F);
         this.eyeHeight = this.spacingY / 2.0F;
     }
 
@@ -25,10 +25,19 @@ public class BeachChairEntity extends Entity implements EntitySpawnDataProvider 
 
     @Override
     public void tick() {
-        this.method_1365(this.velocityX, -0.05, this.velocityZ);
+        this.method_1322(0, -0.05D, 0);
         super.tick();
         this.move(this.velocityX, this.velocityY, this.velocityZ);
+        this.yaw += 1F;
 
+
+        if (this.field_1594 != null && this.field_1594 instanceof PlayerEntity player) {
+            System.out.println("PLAYER");
+            if (((LivingEntityAccessor) player).jumping()) {
+                System.out.println("JUMP");
+                this.method_1322(0, 0.1D, 0);
+            }
+        }
     }
 
     @Override
@@ -63,11 +72,11 @@ public class BeachChairEntity extends Entity implements EntitySpawnDataProvider 
 
     @Override
     public void method_1382() { // updateRiderPosition
-        if (this.field_1594 == null){
+        if (this.field_1594 == null) {
             return;
         }
-        double d = Math.cos((double)this.yaw * Math.PI / 45.0) * 0.0;
-        double d1 = Math.sin((double)this.yaw * Math.PI / 45.0) * 0.0;
+        double d = Math.cos((double) this.yaw * Math.PI / 45.0) * 0.0;
+        double d1 = Math.sin((double) this.yaw * Math.PI / 45.0) * 0.0;
         this.field_1594.method_1340(this.x + d, this.y + this.method_1357() + this.field_1594.method_1385(), this.z + d1);
     }
 
@@ -76,7 +85,7 @@ public class BeachChairEntity extends Entity implements EntitySpawnDataProvider 
             return true;
         }
         if (!this.world.isRemote) {
-            entityplayer.method_1376((Entity)this);
+            entityplayer.method_1376((Entity) this);
         }
         return true;
     }
