c177932fd7ad42ccdc8fe82c943df34ed908c648
Daniel Fildán <fildand@centrum.cz>
2024-05-17 14:54:14 +0200

    Remove some useless scaling

--- src/main/java/net/danygames2014/tropicraft/entity/renderer/BeachChairRenderer.java
+++ src/main/java/net/danygames2014/tropicraft/entity/renderer/BeachChairRenderer.java
@@ -23,9 +23,9 @@ public class BeachChairRenderer extends EntityRenderer {
         GL11.glPushMatrix();
         GL11.glTranslatef((float)x, (float)y - 0.2f, (float)z);
         GL11.glRotatef(180.0f - g, 0.0f, 1.0f, 0.0f);
-        float f4 = 0.75f;
-        GL11.glScalef(f4, f4, f4);
-        GL11.glScalef(1.0f / f4, 1.0f / f4, 1.0f / f4);
+//        float f4 = 0.75f;
+//        GL11.glScalef(f4, f4, f4);
+//        GL11.glScalef(1.0f / f4, 1.0f / f4, 1.0f / f4);
         this.bindTexture("/assets/tropicraft/stationapi/textures/entity/beach_chair/beach_chair_red.png");
         GL11.glScalef(-1.0f, -1.0f, 1.0f);
         this.beachChairModel.render(0.0F,1.0F,0.1F,0.0F,0.0F,0.0625F);
